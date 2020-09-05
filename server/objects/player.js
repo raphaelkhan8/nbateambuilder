54dde48318e8e5bdcd169e34bf853155a9d8cbd1
unknown <raphaelkhan8@gmail.com>
2020-09-05 17:50:40 -0500

    converted playerObj to a class

--- server/objects/player.js
+++ server/objects/player.js
@@ -1,39 +1,40 @@
 const playerData = require("../data/2019-2020_advanced_player_season_totals.json");
 const _ = require("lodash");
 
-const generatePlayerObj = (
-    name,
-    team,
-    position,
-    age,
-    minutes_played,
-    win_shares,
-    win_shares_per_48_minutes,
-    value
-) => {
-    const player = {};
-    player.name = name;
-    player.team = team;
-    player.position = position;
-    player.age = age;
-    player.minutes_played = minutes_played;
-    player.win_shares = win_shares;
-    player.win_shares_per_48_minutes = win_shares_per_48_minutes;
-    player.value = value;
-    return player;
-};
+class Player {
+    constructor(
+        name,
+        team,
+        position,
+        age,
+        minutes_played,
+        win_shares,
+        win_shares_per_48_minutes,
+        value_over_replacement_player
+    ) {
+        this.name = name;
+        this.team = team;
+        this.position = position;
+        this.age = age;
+        this.minutes_played = minutes_played;
+        this.win_shares = win_shares;
+        this.win_shares_per_48_minutes = win_shares_per_48_minutes;
+        this.value_over_replacement_player = value_over_replacement_player;
+    }
+}
 
-const playersArr = playerData.map((player) =>
-    generatePlayerObj(
-        player.name,
-        player.team,
-        player.positions[0].replace(/\s+/g, "_"),
-        player.age,
-        player.minutes_played,
-        player.win_shares,
-        player.win_shares_per_48_minutes,
-        player.value
-    )
+const playersArr = playerData.map(
+    (player) =>
+        new Player(
+            player.name,
+            player.team,
+            player.positions[0].replace(/\s+/g, "_"),
+            player.age,
+            player.minutes_played,
+            player.win_shares,
+            player.win_shares_per_48_minutes,
+            player.value_over_replacement_player
+        )
 );
 
 const playersByPosition = _.groupBy(playersArr, "position");
