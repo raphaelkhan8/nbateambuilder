2fe889ae971247c96182919f5fbcca3774abb50d
unknown <raphaelkhan8@gmail.com>
2020-09-06 11:01:47 -0500

    rendering some players

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Centers from "./Components/Centers";
+import "./App.css";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            centers: [],
+            powerForwards: [],
+            smallForwards: [],
+            shootingGuards: [],
+            pointGuards: [],
+        };
+    }
+
+    componentDidMount() {
+        this.getPlayerList();
+    }
+
+    getPlayerList = () => {
+        return axios
+            .get("/getPlayers")
+            .then((playerList) => {
+                const { data } = playerList;
+                this.setState({
+                    centers: data.CENTER,
+                    powerForwards: data.POWER_FORWARD,
+                    smallForwards: data.SMALL_FORWARD,
+                    shootingGuards: data.SHOOTING_GUARD,
+                    pointGuards: data.POINT_GUARD,
+                });
+            })
+            .catch((err) => console.error(err));
+    };
+
+    render() {
+        const { centers } = this.state;
+
+        return (
+            <div className="App">
+                <h1>Player List</h1>
+                {Object.keys(centers).length ? (
+                    <div id="players">
+                        <h2>CENTERS</h2>
+                        {centers.map((center) => (
+                            <Centers
+                                player={center}
+                                addPlayer={this.addPlayer}
+                            />
+                        ))}
+                    </div>
+                ) : (
+                    <div>
+                        <h2>No List Items Found</h2>
+                    </div>
+                )}
+            </div>
+        );
+    }
 }
 
 export default App;
