c9d5f24111f44ed58b9925165bcd7050eb17e540
unknown <raphaelkhan8@gmail.com>
2020-09-07 16:36:30 -0500

    added cors

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const path = require("path");
 const port = process.env.PORT || 5000;
@@ -7,6 +8,7 @@ const port = process.env.PORT || 5000;
 const { playerRouter } = require("../server/routes/players");
 const { teamRouter } = require("./routes/team");
 
+app.use(cors());
 app.use(express.static(path.join(__dirname, "../public")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
