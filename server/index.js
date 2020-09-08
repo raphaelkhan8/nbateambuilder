bfc455d8db5d9dfae0d87c7412caf79dc6581636
unknown <raphaelkhan8@gmail.com>
2020-09-08 16:07:20 -0500

    cleanup

--- server/index.js
+++ server/index.js
@@ -4,20 +4,21 @@ const cors = require("cors");
 const app = express();
 const path = require("path");
 const port = process.env.PORT || 5000;
+const publicPath = path.join(__dirname, "../build");
 
 const { playerRouter } = require("../server/routes/players");
 const { teamRouter } = require("./routes/team");
 
 app.use(cors());
-app.use(express.static(path.join(__dirname, "../build")));
+app.use(express.static(publicPath));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use(playerRouter);
 app.use(teamRouter);
 
-app.get("/", (req, res) => {
-    res.send("Hello World!");
+app.get("*", (req, res) => {
+    res.sendFile(path.join(publicPath, "index.html"));
 });
 
 app.listen(port, () => {
