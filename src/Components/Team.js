019036d7db5b33746f5b9d3266301424bd0e53da
unknown <raphaelkhan8@gmail.com>
2020-09-12 17:30:48 -0500

    pass unique id

--- src/Components/Team.js
+++ src/Components/Team.js
@@ -1,11 +1,12 @@
 import React from "react";
+import { Button } from "reactstrap";
 
 const Team = (props) => {
-    const { player, releasePlayer } = props;
+    const { player, releasePlayer, id } = props;
     return (
         <table>
             <tbody>
-                <tr>
+                <tr key={id}>
                     <td>
                         <em>{player.name}</em>
                     </td>
@@ -13,14 +14,16 @@ const Team = (props) => {
                     <td>Age: {player.age}</td>
                     <td>Minutes Played: {player.minutes_played}</td>
                     <td>Win Shares: {player.win_shares}</td>
+                    <td>
+                        <Button
+                            onClick={() => {
+                                releasePlayer(player);
+                            }}
+                        >
+                            Release Player
+                        </Button>
+                    </td>
                 </tr>
-                <button
-                    onClick={() => {
-                        releasePlayer(player);
-                    }}
-                >
-                    Release Player
-                </button>
             </tbody>
         </table>
     );
