8f712241aa239135da0f0537a4e4c0eb1f535f17
unknown <raphaelkhan8@gmail.com>
2020-09-25 18:27:18 -0500

    exporting new components

--- src/Components/index.js
+++ src/Components/index.js
@@ -1,5 +1,7 @@
+import InputForm from "./InputForm";
 import Players from "./Players";
 import Team from "./Team";
+import TeamList from "./TeamList";
 import Nav from "./Nav";
 
-export { Players, Team, Nav };
+export { InputForm, Players, Team, TeamList, Nav };
