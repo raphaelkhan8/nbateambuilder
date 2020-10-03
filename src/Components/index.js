3298d607e5ca184df4a6d2683f36b3cfc2577b9b
unknown <raphaelkhan8@gmail.com>
2020-10-03 13:21:16 -0500

    cleaned up App.js

--- src/Components/index.js
+++ src/Components/index.js
@@ -1,7 +1,8 @@
+import HomePage from "./HomePage";
 import InputForm from "./InputForm";
 import Players from "./Players";
 import Team from "./Team";
 import TeamList from "./TeamList";
 import Nav from "./Nav";
 
-export { InputForm, Players, Team, TeamList, Nav };
+export { HomePage, InputForm, Players, Team, TeamList, Nav };
