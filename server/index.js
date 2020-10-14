193177b98a3629805032e457ae48c7befa1c4e0b
unknown <raphaelkhan8@gmail.com>
2020-10-14 01:40:28 -0500

    removed deleted route

--- server/index.js
+++ server/index.js
@@ -7,7 +7,6 @@ const port = process.env.PORT || 5000;
 const publicPath = path.join(__dirname, "../build");
 
 const { playerRouter } = require("../server/routes/players");
-const { teamRouter } = require("./routes/team");
 
 app.use(cors());
 app.use(express.static(publicPath));
@@ -15,7 +14,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use(playerRouter);
-app.use(teamRouter);
 
 app.get("*", (req, res) => {
     res.sendFile(path.join(publicPath, "index.html"));
